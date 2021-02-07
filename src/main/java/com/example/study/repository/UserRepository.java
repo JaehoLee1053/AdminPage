26bddc5a83cbd03a20fa1283207aa4f315501f0f
JaehoLee1053 <dlwogh1053@gmail.com>
2021-02-08 00:04:58 +0900

    20210208_Update UserRepositoryTest

--- src/main/java/com/example/study/repository/UserRepository.java
+++ src/main/java/com/example/study/repository/UserRepository.java
@@ -9,8 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByAccount(String accont);
-
-    Optional<User> findByEmail(String email);
+    User findFirstByPhoneNumberOrderByIdDesc(String phoneNumber);
 
 }
